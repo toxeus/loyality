7c8e8938abb91a66684aba65df70b23efde7e948
Filip Gospodinov <f@gospodinov.ch>
2019-03-23 21:44:25 +0100

    make it prettier

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,13 +1,24 @@
 <template>
+    <div id='outer'><div id='middle'>
     <div id="app">
-        <PrivateKey/>
-        <Balance/>
+        <md-card class="md-primary" md-theme="purple-card" md-with-hover>
+            <md-ripple>
+                <md-card-header>
+                    <div class="md-title"><Balance/></div>
+                </md-card-header>
+                <md-card-content>
+                    <PrivateKey/>
+                </md-card-content>
+            </md-ripple>
+        </md-card>
     </div>
+    </div></div>
 </template>
 
 <script>
 import Balance from './components/Balance.vue'
 import PrivateKey from './components/PrivateKey.vue'
+
 
 export default {
     name: 'app',
@@ -25,6 +36,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 40%;
+  margin: auto;
+  background-color: #ffffff;
 }
+
+#outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+#middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+body {
+  background-color: #f5f5f5;
+}
+
 </style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdCard } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
 
 Vue.config.productionTip = false
 
+Vue.use(MdCard)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
