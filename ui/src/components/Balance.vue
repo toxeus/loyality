3ee1c2276a2a3ff7efed828945f2f263d464b771
Filip Gospodinov <f@gospodinov.ch>
2019-03-23 17:31:53 +0100

    implement balance

--- ui/src/components/Balance.vue
+++ ui/src/components/Balance.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="balance">
         Your balance is:
+        {{ this.balance }}
     </div>
 </template>
 
 <script>
+import Client from 'bitcoin-core'
+
 export default {
     name: 'Balance',
+    data() {
+        return {
+            balance: null,
+            timer: null,
+        }
+    },
+    mounted() {
+        this.timer = setInterval(function () {
+            const client = new Client({ port: 8888, username: 'alice', password: 'bob' });
+            (async () => this.balance = await client.getBalance('"*"'))();
+        }.bind(this), 1000);
+    }
 }
 </script>
 
