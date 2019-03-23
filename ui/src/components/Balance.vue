9c4005e66a6434b4b9fe1d4f51b83b7a3381fc47
Filip Gospodinov <f@gospodinov.ch>
2019-03-23 15:19:51 +0100

    add components

---
+++ ui/src/components/Balance.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="balance">
+        Your balance is:
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Balance',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
